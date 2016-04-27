65afd3a46f0f6c3b1a77e5e55c9c478175e8dd84
John Tran <johnjtran@acm.org>
2016-04-27 12:48:02 -0700

    fix hash

--- snap_hash.sh
+++ snap_hash.sh
@@ -10,19 +10,17 @@ fi
 OUT=${OUTDIR}/hash
 mkdir -p ${OUT}
 
-o Collecting hashes...
+echo Collecting hashes...
 echo OS Type: nix >> $OUT/Hashes.txt
-echo >> $OUT/Hashes.txt
 echo Computername: $hostname >> $OUT/Hashes.txt
-echo >> $OUT/Hashes.txt
 echo Time stamp: `date` >> $OUT/Hashes.txt
-echo >> $OUT/Hashes.txt
-echo ======================MD5 HASHES===================== >> $OUT/Hashes.txt
-echo >> $OUT/Hashes.txt
+
+touch $OUT/md5-hashes.txt
+touch $OUT/sha1-hashes.txt
 
 for f in  $HASH_DIRS; do
   echo $f
-  find $f -type f | xargs -d '\n' md5sum > $OUT/md5-hashes.txt
-  find $f -type f | xargs -d '\n' sha1sum > $OUT/sha1-hashes.txt
+  find $f -type f | xargs -d '\n' md5sum >> $OUT/md5-hashes.txt
+  find $f -type f | xargs -d '\n' sha1sum >> $OUT/sha1-hashes.txt
 done
 
